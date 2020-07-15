0d07a5ea718a291194b0131fc14c9d2e9758216f
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-15 10:54:02 -0500

    version1

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,14 @@
 class Person
-    def initialize (name,account,happiness,hygiene)
+    def initialize (name,account=25,happiness=8,hygiene=8)
         @name = name
-        @account =25
-        @happiness=8
-        @hygiene=8
-    end
+        @account =account
+        @happiness=happiness
+        @hygiene=hygiene
+
     attr_accessor:account
     attr_accessor:happiness
     attr_accessor:hygiene
+
     def clean? (hygiene)
         hygiene>7? "true":"false"
     end
@@ -15,11 +16,17 @@ class Person
         happiness>7? "true":"false"
     end
 
-    def get_paid
+    def get_paid(salary)
+        @account+=salary
+        return "all about the benjamins"
     end
     def take_bath
+        @hygiene+=4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
     def work_out
+        @happiness+=2
+        return "♪ another one bites the dust ♫"
     end
     def call_friend
     end
