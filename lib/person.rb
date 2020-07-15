a37f24c3b0b0ded6af661a72d147e89ec8e2df57
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-15 10:39:51 -0500

    version1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
-# your code goes here
+class Person
+    def initialize (name,account,happiness,hygiene)
+        @name = name
+        @account =25
+        @happiness=8
+        @hygiene=8
+    end
+    attr_accessor:account
+    attr_accessor:happiness
+    attr_accessor:hygiene
+    def clean? (hygiene)
+        hygiene>7? "true":"false"
+    end
+    def happy?(happiness)
+        happiness>7? "true":"false"
+    end
+
+    def get_paid
+    end
+    def take_bath
+    end
+    def work_out
+    end
+    def call_friend
+    end
+    def start_conversation(person, topic)
+    end
+
+end
+
